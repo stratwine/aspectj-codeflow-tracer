741a3c384af487e6c428b4edcf1b721c39241fc0
Vishwanath <stratwine@vishwanath.(none)>
2011-10-22 22:47:45 +0530

    prints arg values and return values

--- Trace.java
+++ Trace.java
@@ -2,17 +2,40 @@
  * The Trace aspect injects tracing messages before and after
  * method main of class HelloWorld.
  */
+
 package helloworld;
 
+
+import org.aspectj.lang.*;
+import org.aspectj.lang.reflect.*;
 aspect Trace  {
 
-  pointcut printouts():
-	execution(public * *(..));
+static final void println(String s){ System.out.println(s); }
 
-  before(): printouts() {
-    System.out.println("#>"+thisEnclosingJoinPointStaticPart);
-  }
-  after():  printouts() {
-    System.out.println("#<"+thisEnclosingJoinPointStaticPart);
-  }
+   pointcut demoExecs(): execution(public * *(..));
+
+   Object around(): demoExecs()  {
+      println("Intercepted message: " +
+          thisJoinPointStaticPart.getSignature().getName());
+      println("in class: " +
+          thisJoinPointStaticPart.getSignature().getDeclaringType().getName());
+      printParameters(thisJoinPoint);
+      println("Running original method: \n" );
+      Object result = proceed();
+      println("  result: " + result );
+      return result;
+   }
+
+   static private void printParameters(JoinPoint jp) {
+      println("Arguments: " );
+      Object[] args = jp.getArgs();
+      String[] names = ((CodeSignature)jp.getSignature()).getParameterNames();
+      Class[] types = ((CodeSignature)jp.getSignature()).getParameterTypes();
+      for (int i = 0; i < args.length; i++) {
+         println("  "  + i + ". " + names[i] +
+             " : " +            types[i].getName() +
+             " = " +            args[i]);
+      }
+   }
+
 }
